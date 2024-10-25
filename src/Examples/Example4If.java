3d92cbdf7d05c3eb5406ec5b1f982c23569cf1f5
Felipe Cardoso <felipe.rcardoso@embraer.com.br>
2024-10-25 14:24:29 -0300

    Arrumando identações

--- src/Examples/Example4If.java
+++ src/Examples/Example4If.java
@@ -7,15 +7,13 @@ public class Example4If {
         Scanner sc = new Scanner(System.in);
         int horas = sc.nextInt();
 
-        if(horas > 7 && horas < 12) {
+        if(horas > 7 && horas < 12)
             System.out.println("Bom dia");
-        } else {
-            if(horas > 12 && horas < 18){
-                System.out.println("Boa tarde");
-            }else {
-                System.out.println("Boa noite");
-            }
-        }
+        else if(horas > 12 && horas < 18)
+            System.out.println("Boa tarde");
+        else
+            System.out.println("Boa noite");
+
         sc.close();
     }
 }
