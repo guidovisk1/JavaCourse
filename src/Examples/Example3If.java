60d3626b3cc30de8339a65c641e9fb8a88ee3208
Felipe Cardoso <felipe.rcardoso@embraer.com.br>
2024-10-25 14:10:44 -0300

    Exemplo condição

---
+++ src/Examples/Example3If.java
@@ -0,0 +1,13 @@
+package Examples;
+
+public class Example3If {
+    public static void main(String[] args) {
+        int numero = 2;
+
+        if(numero != 2) {
+            System.out.println("Número diferente de 2");
+        } else {
+            System.out.println("Número 2");
+        }
+    }
+}
