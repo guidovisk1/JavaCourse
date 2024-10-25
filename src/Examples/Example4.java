b0c74449a4f609c526946a6a117cebf74a43b9ae
Felipe Cardoso <felipe.rcardoso@embraer.com.br>
2024-10-25 15:18:36 -0300

    Example 4 pdf2

---
+++ src/Examples/Example4.java
@@ -0,0 +1,18 @@
+package Examples;
+
+import java.util.Scanner;
+
+public class Example4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int minutos = sc.nextInt();
+        double conta = 50;
+        if(minutos > 100){
+            conta = conta + (minutos - 100) * 2;
+         }
+        System.out.printf("Valor da conta a apagar %.2f", conta);
+
+        sc.close();
+    }
+}
