beaa3d54cf355a9513be2688364274fa81fca041
Felipe Cardoso <felipe.rcardoso@embraer.com.br>
2024-10-25 15:32:03 -0300

    Exemplo de operações ternárias

---
+++ src/Examples/Example5Ternaria.java
@@ -0,0 +1,11 @@
+package Examples;
+
+public class Example5Ternaria {
+    public static void main(String[] args) {
+        int x = 10;
+        double desconto = (x < 2) ? x * 0.1 : x * 0.2;
+
+        System.out.println("Exemplo de operações ternárias");
+
+    }
+}
